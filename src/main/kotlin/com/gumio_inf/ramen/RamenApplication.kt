134df673c2f1b2098c54257029c02d21351df67f
Gumio <quran.gumi3134@gmail.com>
2017-06-03 20:05:09 +0900

    pageable廃止

--- src/main/kotlin/com/gumio_inf/ramen/RamenApplication.kt
+++ src/main/kotlin/com/gumio_inf/ramen/RamenApplication.kt
@@ -5,21 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-class RamenApplication /* {
+class RamenApplication
 
-   @Autowired val ramenRepository: RamenRepository? = null
-
-    override fun run(vararg args: String?) {
-        // データ追加
-        val created: Ramen? = ramenRepository?.save(Ramen(null, "Gumioo", "Gumimi"))
-        println("${created} is created")
-        // ページング処理
-        val pageable = PageRequest(0, 3)
-        val page = ramenRepository?.findAll(pageable)
-        // データ表示
-        ramenRepository?.findAll()?.forEach { println(it) }
-    }
-}*/
 fun main(args: Array<String>) {
     SpringApplication.run(RamenApplication::class.java, *args)
 }
